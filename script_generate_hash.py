5b1848ce2eae186786b6c950a39bd55eac0fefd6
Sourav Kumar <106806869+Sourav072@users.noreply.github.com>
2024-12-20 21:46:45 +0530

    Update script_generate_hash.py

--- script_generate_hash.py
+++ script_generate_hash.py
@@ -1,10 +1,1 @@
-import hashlib
 
-# Path to your script file
-script_path = "url_safety_analyzer.py"
-
-with open(script_path, "rb") as f:
-    script_content = f.read()
-
-script_hash = hashlib.sha256(script_content).hexdigest()
-print(f"Script Hash: {script_hash}")
